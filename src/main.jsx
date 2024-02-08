fdf9c43bfe9854dbe49fd99619dd8035f0786342
Alexander Selitskiy <116916029+seinsdieum@users.noreply.github.com>
2024-02-08 17:12:47 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import {store} from "./store/index.js";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter base="/scheduler/">
         <App />
     </BrowserRouter>
 </Provider>
