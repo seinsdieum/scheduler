693f394d48cd19a3a5bf46738cdb3036e6017121
Alexander Selitskiy <116916029+seinsdieum@users.noreply.github.com>
2024-02-08 17:54:43 +0300

    Update schedule.js

--- src/data/schedule.js
+++ src/data/schedule.js
@@ -5,7 +5,7 @@ const createElement = (time, name, auditory, teacher, type, weekRelation) => {
 const mondaySchedule = [
     createElement('14:40', 'Мат прога', '200-3a', 'Бурмакова', 'лк'),
     createElement('16:30', 'ТРПИ', '301-4', 'Кудлацкая', 'лк'),
-    createElement('18:05', 'Политхуйня', '239-4', 'хз', 'пз', 1),
+    createElement('18:05', 'Шизоэкономия', '239-4', 'хз', 'пз', 1),
     createElement('18:05', 'Дизайн', '202-4', 'хз', 'лр', 2),
 ]
 
@@ -18,20 +18,20 @@ const tuesdaySchedule = [
 
 const wednesdaySchedule = [
     createElement('14:40', 'Физра', 'Зал', ',ehvfrjdf', 'лк'),
-    createElement('14:40', 'Политхуйня', '401-4', 'Новикова', 'лк'),
+    createElement('14:40', 'Шизоэкономия', '401-4', 'Новикова', 'лк'),
     createElement('18:05', 'Мат прога', '202-4', 'хз', 'лр'),
     createElement('19:40', 'ТПВИ', '204-1', ',ehvfrjdf', 'лр' ),
 ]
 const thirsdaySchedule = [
-    createElement('14:40', 'Комп гео', '324-1', 'хз', 'лр'),
-    createElement('16:30', 'Мат прога', '200-3a', ',ehvfrjdf', 'лк', 1),
-    createElement('16:30', 'Комп гео q', '200-3a', ',ehvfrjdf', 'лк', 2),
-    createElement('14:40', 'Мат прога', '200-3a', ',ehvfrjdf', 'пз', 1),
-    createElement('14:40', 'Мат прога', '200-3a', ',ehvfrjdf', 'лр', 2),
+    createElement('14:40', 'Комп гео', '324-1', 'Дятко', 'лр'),
+    createElement('16:30', 'Комп гео', 'хз-где', 'Дятко', 'лк', 1),
+    createElement('16:30', 'РиАТ', '132-4', ',ehvfrjdf', 'лк', 2),
+    createElement('18:05', 'ООП', '132-4', 'Мущук', 'лк'),
+    createElement('19:40', 'ТПВИ', 'непомню', 'крутой перец', 'лр'),
 ]
 
 const fridaySchedule = [
-    createElement('14:40', 'Мат прога', '200-3a', ',ehvfrjdf', 'лк'),
+    createElement('14:40', 'Физра', 'Зал епт', 'решайте сами', 'лк'),
     createElement('14:40', 'Мат прога', '200-3a', ',ehvfrjdf', 'лк'),
     createElement('14:40', 'Мат прога', '200-3a', ',ehvfrjdf', 'пз', 1),
     createElement('14:40', 'Мат прога', '200-3a', ',ehvfrjdf', 'лр', 2),
