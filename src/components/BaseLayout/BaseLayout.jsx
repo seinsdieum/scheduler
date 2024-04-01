891e12c4f45be6722faa4a93d6d3e6382fc05903
Alex Ellipse <alexellipse@icloud.com>
2024-04-01 19:11:07 +0300

    PWA Test

--- src/components/BaseLayout/BaseLayout.jsx
+++ src/components/BaseLayout/BaseLayout.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import style from './BaseLayout.module.css'
 import {Outlet} from "react-router-dom";
 import githubSrc from './github-mark-white.png'
+import Icon from "../Icon/Icon.jsx";
+import fitSrc from '../../../icons/fit.svg'
 const BaseLayout = () => {
     return (
         <div className={style.base__layout}>
+            <header>
+                <Icon src={fitSrc}></Icon>
+                <h2>scheduler</h2>
+            </header>
             <main>
                 <Outlet></Outlet>
             </main>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
+
+
+const manifestForPlugIn = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"Расписание ФИТ-2-5",
+    short_name:"Расписание",
+    description:"Список занятий для 5 группы 1 подгруппы БГТУ. PWA пока что в тестовом режиме",
+    icons:[{
+      src: '/android-chrome-192x192.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+      {
+        src:'/android-chrome-512x512.png',
+        sizes:'512x512',
+        type:'image/png',
+        purpose:'favicon'
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes:'180x180',
+        type:'image/png',
+        purpose:'apple touch icon',
+      },
+      {
+        src: '/maskable_icon.png',
+        sizes:'512x512',
+        type:'image/png',
+        purpose:'any maskable',
+      }
+    ],
+    theme_color:'#8CDC8CFF',
+    background_color:'#141914FF',
+    display:"standalone",
+    scope:'/',
+    start_url:"/",
+    orientation:'portrait'
+  }
+}
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   base: '/scheduler/'
 })
