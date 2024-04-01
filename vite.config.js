12ee21cb8fc0d67d44036c28011bf553a0329e3a
Alex Ellipse <alexellipse@icloud.com>
2024-04-02 00:25:44 +0300

    PWA Test, made beautiful

--- vite.config.js
+++ vite.config.js
@@ -7,31 +7,31 @@ import { VitePWA } from "vite-plugin-pwa";
 
 const manifestForPlugIn = {
   registerType:'prompt',
-  includeAssests:['icons/fit.svg', "apple-touc-icon.png", "masked-icon.svg"],
+  includeAssests:['favicons/BSTU_180.png','favicons/BSTU_192.png','favicons/BSTU_512.png'],
   manifest:{
     name:"Расписание ФИТ-2-5",
     short_name:"Расписание",
     description:"Список занятий для 5 группы 1 подгруппы БГТУ. PWA пока что в тестовом режиме",
     icons:[{
-      src: '/android-chrome-192x192.png',
+      src: 'favicons/BSTU_192.png',
       sizes:'192x192',
       type:'image/png',
       purpose:'favicon'
     },
       {
-        src:'/android-chrome-512x512.png',
+        src:'favicons/BSTU_512.png',
         sizes:'512x512',
         type:'image/png',
         purpose:'favicon'
       },
       {
-        src: '/apple-touch-icon.png',
+        src: 'favicons/BSTU_180.png',
         sizes:'180x180',
         type:'image/png',
         purpose:'apple touch icon',
       },
       {
-        src: '/maskable_icon.png',
+        src: 'favicons/BSTU_512.png',
         sizes:'512x512',
         type:'image/png',
         purpose:'any maskable',
